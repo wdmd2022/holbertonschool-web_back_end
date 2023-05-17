3f716ba3b6e6d80e4a940ec56fc935972ae11044
Douglas <wdmdavison@icloud.com>
2023-05-17 04:34:59 +0000

    upload 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,16 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+  // needs a getter for each attribute but the sqft getter is in the super //
+  get floors() {
+    return (this._floors);
+  }
+  evacuationWarningMessage() {
+    return (`evacuate slowly the ${this._floors} floors`);
+  }
+}
+
