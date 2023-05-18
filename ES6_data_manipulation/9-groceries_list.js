cef32d5231b834909df182266c9b52fe4b377132
Douglas <wdmdavison@icloud.com>
2023-05-18 03:46:27 +0000

    upload 9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+  const listEnUp = new Map();
+
+  listEnUp.set('Apples', 10);
+  listEnUp.set('Tomatoes', 10);
+  listEnUp.set('Pasta', 1);
+  listEnUp.set('Rice', 1);
+  listEnUp.set('Banana', 5);
+
+  return listEnUp;
+}
