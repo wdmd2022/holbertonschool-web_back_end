14eca61182458b32c2883387bd098766a85596ac
Douglas <wdmdavison@icloud.com>
2023-09-06 18:01:38 +0000

    upload 0 with docstrings

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
+"""this module contains the add function
+"""
+
+
 def add(a: float, b: float) -> float:
+    """This function adds two floats, which is cool
+
+    Args:
+        a (float): the first number to add
+        b (float): the second number to add
+
+    Returns:
+        float: the sum of the two numbers
+    """
     return a + b
