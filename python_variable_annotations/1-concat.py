628fa86bf0152f7af7713fa2130f5772059fb77b
Douglas <wdmdavison@icloud.com>
2023-09-06 18:07:05 +0000

    upload 1 for testing

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" this module contains a function that concatentates 2 strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """this function concatenates two strings
+
+    Args:
+        str1 (str): the first string to concatenate
+        str2 (str): the second string to concatenate
+
+    Returns:
+        str: the finished concatenated string
+    """
+    return str1 + str2
