25cbd6029d864008ac1f6a4bb089e683a5fec49f
Douglas <wdmdavison@icloud.com>
2023-09-06 18:18:29 +0000

    upload 3 for testing

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/user/bin/env python3
+"""this module contains a function to cast floats to strings
+"""
+
+
+def to_str(n: float) -> str:
+    """this function turns floats into their string representation
+
+    Args:
+        n (float): the number we wish to turn into a string
+
+    Returns:
+        str: the string representation of the number
+    """
+    return str(n)
