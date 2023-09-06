d2427b67387774152abb551a22895ff600dcd1f4
Douglas <wdmdavison@icloud.com>
2023-09-06 18:25:24 +0000

    upload 4 for testing

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
-"""this module 
+"""this module defines a bunch of variables for us with specified types
+a, an integer with a value of 1
+pi, a float with a value of 3.14
+i_understand_annotations, a boolean with a value of True
+school, a string with a value of “Holberton”
 """
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
