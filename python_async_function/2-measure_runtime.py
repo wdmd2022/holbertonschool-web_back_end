02feed407c09894940d702606febf7b01ccb7032
Douglas <wdmdavison@icloud.com>
2023-09-07 17:59:45 +0000

    upload 2 with comments and styling

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,7 +8,22 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    async def nested_async_wait():
+    """this function is a synchronous function that uses a nested event loop
+    to run asynchronous code whose execution time we measure and return
+
+    Args:
+        n (int): number of times wait_n will execute
+        max_delay (int): delay variable for setting range of wait times on
+        wait_n executions
+
+    Returns:
+        float: seconds it took to run the async code
+    """
+    async def nested_async_wait() -> None:
+        """nested async function definition which thereby allows us to call
+        wait_n function, which is asynchronous, from the synchronous
+        measure_time function we will ultimately import into our main file
+        """
         await wait_n(n, max_delay)
     start_time = perf_counter()
     loop = asyncio.new_event_loop()
