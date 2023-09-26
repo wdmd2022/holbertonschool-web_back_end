c0711a8c2761cbd4ea0b7f4b29ee1be75d50bfcf
Douglas <wdmdavison@icloud.com>
2023-09-26 18:41:05 +0000

    test basic auth

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ class that extends Auth to provide a different type of auth"""
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ returns the Base64 part of the Authorization header.
+        You can assume authorization_header contains only one Basic """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
