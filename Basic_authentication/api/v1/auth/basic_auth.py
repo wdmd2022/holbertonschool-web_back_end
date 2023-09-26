e259c6a96d7b4d8acfba176fd9efe5df737b9341
Douglas <wdmdavison@icloud.com>
2023-09-26 18:53:11 +0000

    upload decoder

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,16 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ this method returns the decoded value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            llcoolvalue = base64.b64decode(base64_authorization_header)
+            return llcoolvalue.decode('utf-8')
+        except base64.binascii.Error:
+            return None
