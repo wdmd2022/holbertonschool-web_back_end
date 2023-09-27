0cbcb68fdd88d2ef49eb661a06131d6f58db7261
Douglas <wdmdavison@icloud.com>
2023-09-27 17:10:00 +0000

    upload 6 for testing

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 
 
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -23,3 +24,16 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns a User instance based on a cookie value"""
+        if request is None:
+            return None
+        cool_session_id = self.session_cookie(request)
+        if cool_session_id is None:
+            return None
+        cool_user_id = self.user_id_for_session_id(cool_session_id)
+        if cool_user_id is None:
+            return None
+        cool_user = User.get(cool_user_id)
+        return cool_user
