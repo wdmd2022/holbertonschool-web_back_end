5c132284d000d946b6d55b332233e9dd4ea28c7d
Douglas <wdmdavison@icloud.com>
2023-09-28 03:06:37 +0000

    testing flask endpoints

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,16 +2,31 @@
 """ this file initializes a flask app for us """
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET'], strict_slashes=False)
 def welcome():
     """this basically says 'howdy' in French"""
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users():
+    """this is the endpoint to register a user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        new_user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
