6c0a079083a19cdcf0a85ec45e83b0e354784a0f
Douglas <wdmdavison@icloud.com>
2023-09-28 03:19:55 +0000

    upload some auth tasks for testing

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 class Auth:
@@ -26,6 +27,21 @@ class Auth:
             hashed_pass = _hash_password(password)
             confirmed_new_user = self._db.add_user(email, hashed_pass)
             return confirmed_new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ this method uses bcrypt to check a user's password"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(
+                'utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
+
+
+def _generate_uuid() -> str:
+    """ this private little function generates a new uuid for us, using
+    uuid4's random uuid generator"""
+    return str(uuid4)
 
 
 def _hash_password(password: str) -> bytes:
