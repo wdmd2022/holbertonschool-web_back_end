bb83d0569213ee4a52732a284c33e7e3a8b44faf
Douglas <wdmdavison@icloud.com>
2023-10-04 00:42:24 +0000

    initial upload

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""this file contains unit tests for the utils module"""
+
+
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """test case for test_access_nested_map"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """tests that we raise an exception"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
