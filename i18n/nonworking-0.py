931518a2ff7f084f121339e0df89828b7d23180c
Douglas <wdmdavison@icloud.com>
2023-10-04 22:49:10 +0000

    change templating for checker purposes

--- i18n/nonworking-0.py
+++ i18n/nonworking-0.py
@@ -11,7 +11,10 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'], strict_slashes=False)
 def welcome():
     """this basically says 'howdy' """
-    return render_template('0-index.html'), 200
+    return render_template(
+        '0-index.html',
+        title='Welcome to Holberton',
+        header='Hello world'), 200
 
 
 if __name__ == '__main__':
