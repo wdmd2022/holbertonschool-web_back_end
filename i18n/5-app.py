488bd45ea8efb8997c8f0fa25fa8055dc3e1bdf0
Douglas <wdmdavison@icloud.com>
2023-10-05 04:01:17 +0000

    test 5

---
+++ i18n/5-app.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""a simple flask app for demonstrating i18n"""
+
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config():
+    """we configure our Babel languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """grabs the lcoale when a request is made"""
+    url_locale = request.args.get('locale')
+    if url_locale and url_locale in Config.LANGUAGES:
+        return url_locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def welcome():
+    """this basically says 'howdy' """
+    return render_template('5-index.html'), 200
+
+
+def get_user():
+    """this checks for the user in the db (or mock db like here)"""
+    url_user = request.args.get('login_as')
+    if url_user and url_user in users:
+        return users['url_user']
+    return None
+
+
+@app.before_request
+def before_request():
+    """if there is a user, here is where we'll set it globally on g"""
+    g.user = get_user()
+
+
+if __name__ == '__main__':
+    app.run()
