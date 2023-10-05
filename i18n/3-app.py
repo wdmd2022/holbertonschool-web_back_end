d08ddd27e72cb85bfab96a699372f581fbed330a
Douglas <wdmdavison@icloud.com>
2023-10-05 01:44:31 +0000

    not working

---
+++ i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""a simple flask app for demonstrating i18n"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config():
+    """we configure our Babel languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """grabs the lcoale when a request is made"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def welcome():
+    """this basically says 'howdy' """
+    return render_template('3-index.html'), 200
+
+
+if __name__ == '__main__':
+    app.run()
