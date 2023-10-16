bd0d0869ac7554570c2e584ea11d47410babd09e
Douglas <wdmdavison@icloud.com>
2023-10-16 00:23:10 +0000

    add docstrings

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -13,6 +13,7 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """method to store data in our redis cache"""
         new_key = str(uuid.uuid4())
         self._redis.set(new_key, data)
         return new_key
@@ -20,12 +21,15 @@ class Cache:
     def get(self,
             key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """method to get data from redis and optionally type cast"""
         if fn:
             return fn(self._redis.get(key))
         return self._redis.get(key)
 
     def get_str(self, key: str) -> str:
+        """method to parameterize get for returning strings"""
         return self.get(key, str)
 
     def get_int(self, key: str) -> str:
+        """method to parameterize get for returning ints"""
         return self.get(key, int)
