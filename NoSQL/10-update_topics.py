4ac2e3b875ecbf22f0511e7716bf1c003ff44f64
Douglas <wdmdavison@icloud.com>
2023-10-17 19:14:42 -0700

    upload 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""script to update specific school records, re: their topics
+`mongo_collection` will be the pymongo collection object
+`name` (string) will be the school name to update
+`topics` (list of strings) will be the list of topics approached in the school
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """updates a specific record, by name, with new topics"""
+    mongo_collection.update_one({"name": name},
+                                {"$set": {"topics": topics}})
