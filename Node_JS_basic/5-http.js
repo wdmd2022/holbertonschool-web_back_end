5d44afc94f5f48d4f02f41627925a7c650c1ed6d
Douglas <wdmdavison@icloud.com>
2023-10-31 22:20:46 +0000

    incorporate proper return into server + upload readme for unit tests

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,59 @@
+// creates a more complex server using the http module
+
+const http = require('http');
+
+const coolDataBase = process.argv[2];
+const hostname = '127.0.0.1';
+const port = 1245;
+const fs = require('fs').promises;
+
+let returnString = '';
+
+async function countStudents(csvPath) {
+  let content;
+  try {
+    content = await fs.readFile(csvPath, 'utf-8');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('Cannot load the database');
+    }
+  }
+  const arrayOfLines = content.split('\n').filter(Boolean); // clever bool
+  arrayOfLines.shift(); // we remove the header lines
+  let studentCount = 0;
+  const studentCountByField = {};
+  const studentNamesByField = {};
+  arrayOfLines.forEach((line) => {
+    const [firstName, , , field] = line.split(',');
+    if (!studentCountByField[field]) {
+      studentCountByField[field] = 0;
+      studentNamesByField[field] = [];
+    } // this is for if we haven't seen the field yet
+    studentCountByField[field] += 1;
+    studentNamesByField[field].push(firstName);
+    studentCount += 1;
+  });
+  returnString = returnString.concat(`Number of students: ${studentCount}\n`);
+  for (const [field, count] of Object.entries(studentCountByField)) {
+    returnString = returnString.concat(
+      `Number of students in ${field}: ${count}. List: ${studentNamesByField[field].join(', ')}\n`,
+    );
+  }
+  return returnString;
+}
+
+const server = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    const coolStudents = await countStudents(coolDataBase);
+    res.end(`This is the list of our students\n${coolStudents}`);
+  }
+});
+
+const app = server.listen(port, hostname);
+
+module.exports = app;
