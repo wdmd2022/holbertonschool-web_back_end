b76aed004251fa140869bf32b63e215dc13b13cb
Douglas <wdmdavison@icloud.com>
2023-10-31 22:44:15 +0000

    upload express

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,1 +1,12 @@
 // creates a small Express server
+
+const express = require('express');
+let port = 1245;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!')
+})
+
+app.listen(port);
