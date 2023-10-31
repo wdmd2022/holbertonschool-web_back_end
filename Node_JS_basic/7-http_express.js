a4633047e5e2cf50001419959d5f916fc7de68c5
Douglas <wdmdavison@icloud.com>
2023-10-31 22:55:19 +0000

    upload more complex express

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,57 @@
+// creates a small Express server
+
+const fs = require('fs').promises;
+const express = require('express');
+
+const port = 1245;
+const database = process.argv[2];
+const app = express();
+
+async function countStudents(csvPath) {
+  let content;
+  try {
+    content = await fs.readFile(csvPath, 'utf-8');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('Cannot load the database');
+    }
+  }
+  const arrayOfLines = content.split('\n').filter(Boolean); // clever bool
+  arrayOfLines.shift(); // we remove the header lines
+  let studentCount = 0;
+  const studentCountByField = {};
+  const studentNamesByField = {};
+  let returnString = '';
+  arrayOfLines.forEach((line) => {
+    const [firstName, , , field] = line.split(',');
+    if (!studentCountByField[field]) {
+      studentCountByField[field] = 0;
+      studentNamesByField[field] = [];
+    } // this is for if we haven't seen the field yet
+    studentCountByField[field] += 1;
+    studentNamesByField[field].push(firstName);
+    studentCount += 1;
+  });
+  returnString = returnString.concat(`Number of students: ${studentCount}\n`);
+  for (const [field, count] of Object.entries(studentCountByField)) {
+    returnString = returnString.concat(
+      `Number of students in ${field}: ${count}. List: ${studentNamesByField[field].join(', ')}\n`,
+    );
+  }
+  return returnString;
+}
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const studentData = await countStudents(database);
+    res.status(200).send(`This is the list of our students\n${studentData}`);
+  } catch (error) {
+    res.status(200).send(`This is the list of our students\n${error.message}`);
+  }
+});
+
+app.listen(port);
