a7a8267b12afe49861ecef441c9b9b6f936b4673
Douglas <wdmdavison@icloud.com>
2023-11-02 03:14:18 +0000

    reinsert old tests

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -22,3 +22,19 @@ describe('Cart page', function() {
     });
 
 });
+
+describe('test suite for index page', function() {
+    it('should return 200 when everything is all good', function(done) {
+        request.get('http://localhost:7865', function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('should welcome us warmly', function(done) {
+        request.get('http://localhost:7865', function(error, response, body) {
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+});
