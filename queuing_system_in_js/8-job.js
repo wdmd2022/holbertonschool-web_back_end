0cd5fa281799cd4d49f51aebd1aec2607c9d73f2
Douglas <wdmdavison@icloud.com>
2023-11-02 15:55:17 +0000

    upload 8

---
+++ queuing_system_in_js/8-job.js
@@ -0,0 +1,20 @@
+// holds our function createPushNoticationsJobs
+
+
+function createPushNotificationsJobs(jobs, queue) {
+    if (!Array.isArray(jobs)) {
+        throw new Error('Jobs is not an array');
+    }
+    jobs.forEach(item => {
+        const newJob = queue.create('push_notification_code_3', item);
+        newJob.save((err) => {
+            if (!err) {
+                console.log(`Notification job created: ${newJob.id}`);
+            }
+        });
+        newJob.on('failed', (err) => console.log(`Notification job ${newJob.id} failed: ${err}`));
+        newJob.on('complete', () => console.log(`Notification job ${newJob.id} completed`));
+        newJob.on('progress', (progress) => console.log(`Notification job ${newJob.id} ${progress}% complete`));
+    });
+}
+module.exports = createPushNotificationsJobs;
